1d958671e57253441befdca2c07b4332350494a7
QuorZak <zakquor@gmail.com>
2024-10-29 19:01:58 +1300

    Aruco markers pose collected

--- src/tests.cpp
+++ src/tests.cpp
@@ -217,59 +217,73 @@ void generateMarker() {
 
 // function to start up camera rgb, find specifically the marker generated in generate marker, and display it
 void findMarkerAndPose() {
-  using namespace cv;
-  // Initialize the RealSense pipeline
-  rs2::pipeline pipe;
-  rs2::config config;
-  config.enable_stream(RS2_STREAM_COLOR, cam_res_width, cam_res_height, RS2_FORMAT_BGR8, cam_fps); // Use global parameters
-  pipe.start(config);
+using namespace cv;
+// Initialize the RealSense pipeline
+rs2::pipeline pipe;
+rs2::config config;
+config.enable_stream(RS2_STREAM_COLOR, cam_res_width, cam_res_height, RS2_FORMAT_BGR8, cam_fps); // Use global parameters
+pipe.start(config);
 
-  // Load the marker image
-  cv::Mat marker = cv::imread(f_markers_location + f_marker_name, cv::IMREAD_COLOR);
+// Get the intrinsics of the color stream
+rs2::pipeline_profile profile = pipe.get_active_profile();
+auto color_stream = profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+rs2_intrinsics intrinsics = color_stream.get_intrinsics();
 
-  while (true) {
-    // Get the frames
-    auto frames = pipe.wait_for_frames();
-    auto color_frame = frames.get_color_frame();
+// Camera matrix and distortion coefficients
+cv::Mat camMatrix = (cv::Mat_<double>(3, 3) << intrinsics.fx, 0, intrinsics.ppx, 0, intrinsics.fy, intrinsics.ppy, 0, 0, 1);
+cv::Mat distCoeffs = (cv::Mat_<double>(5, 1) << intrinsics.coeffs[0], intrinsics.coeffs[1], intrinsics.coeffs[2], intrinsics.coeffs[3], intrinsics.coeffs[4]);
 
-    // Convert the frame to an OpenCV image
-    cv::Mat frame(cv::Size(cam_res_width, cam_res_height), CV_8UC3, const_cast<void*>(color_frame.get_data()), cv::Mat::AUTO_STEP);
+  // set coordinate system
+  float markerLength = 0.1f;
+  cv::Mat objPoints(4, 1, CV_32FC3);
+  objPoints.ptr<Vec3f>(0)[0] = Vec3f(-markerLength/2.f, markerLength/2.f, 0);
+  objPoints.ptr<Vec3f>(0)[1] = Vec3f(markerLength/2.f, markerLength/2.f, 0);
+  objPoints.ptr<Vec3f>(0)[2] = Vec3f(markerLength/2.f, -markerLength/2.f, 0);
+  objPoints.ptr<Vec3f>(0)[3] = Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
 
-    // Detect the marker
-    std::vector<int> markerIds;
-    std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-    cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
-    cv::aruco::Dictionary dictionary = getPredefinedDictionary(cv::aruco::DICT_5X5_250);
-    cv::aruco::ArucoDetector detector(dictionary, detectorParams);
-    detector.detectMarkers(frame, markerCorners, markerIds, rejectedCandidates);
+while (true) {
+  // Get the frames
+  auto frames = pipe.wait_for_frames();
+  auto color_frame = frames.get_color_frame();
 
-    size_t nMarkers = markerCorners.size();
-    std::vector<Vec3d> rvecs(nMarkers), tvecs(nMarkers);
+  // Convert the frame to an OpenCV image
+  cv::Mat frame(cv::Size(cam_res_width, cam_res_height), CV_8UC3, const_cast<void*>(color_frame.get_data()), cv::Mat::AUTO_STEP);
 
-    if(estimatePose && !markerIds.empty()) {
-      // Calculate pose for each marker
-      for (size_t i = 0; i < nMarkers; i++) {
-        solvePnP(objPoints, markerCorners.at(i), camMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
-      }
-    }
+  // Detect the marker
+  std::vector<int> markerIds;
+  std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+  cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+  cv::aruco::Dictionary dictionary = getPredefinedDictionary(cv::aruco::DICT_5X5_250);
+  cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+  detector.detectMarkers(frame, markerCorners, markerIds, rejectedCandidates);
 
-    // If the marker is detected, draw the marker
-    if (!markerIds.empty()) {
-      cv::aruco::drawDetectedMarkers(frame, markerCorners, markerIds);
-      for(unsigned int i = 0; i < markerIds.size(); i++)
-        cv::drawFrameAxes(frame, camMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 1.5f, 2);
-    }
+  size_t nMarkers = markerCorners.size();
+  std::vector<Vec3d> rvecs(nMarkers), tvecs(nMarkers);
 
-    // Display the frame
-    cv::imshow("Frame", frame);
-
-    // If the user presses a key
-    if (cv::waitKey(1) >= 0) {
-      break;
+  if(estimatePose && !markerIds.empty()) {
+    // Calculate pose for each marker
+    for (size_t i = 0; i < nMarkers; i++) {
+      solvePnP(objPoints, markerCorners.at(i), camMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
     }
   }
-  pipe.stop();
-  destroyAllWindows();
+
+  // If the marker is detected, draw the marker
+  if (!markerIds.empty()) {
+    cv::aruco::drawDetectedMarkers(frame, markerCorners, markerIds);
+    for(unsigned int i = 0; i < markerIds.size(); i++)
+      cv::drawFrameAxes(frame, camMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 1.5f, 2);
+  }
+
+  // Display the frame
+  cv::imshow("Frame", frame);
+
+  // If the user presses a key
+  if (cv::waitKey(1) >= 0) {
+    break;
+  }
+}
+pipe.stop();
+destroyAllWindows();
 }
 
 
